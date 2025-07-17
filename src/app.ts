5654a3739d8cc7cf0b0ba5fc98184c305002dd63
lovanto <rifkylovanto@gmail.com>
2025-07-17 15:27:34 +0700

    fix the cors maybe

--- src/app.ts
+++ src/app.ts
@@ -22,16 +22,20 @@ const allowedOrigins = config.corsOrigin.split(',').map(origin => origin.trim())
 app.use(rateLimiter);
 app.use(timeout('10s'));
 app.use(helmet());
+
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        return callback(new Error('Not allowed by CORS'));
       }
     },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 
