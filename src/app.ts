ab152f1a414f2d5aae8b6092c1936e28598a832a
lovanto <rifkylovanto@gmail.com>
2025-07-17 15:49:55 +0700

    restore cors settings

--- src/app.ts
+++ src/app.ts
@@ -22,20 +22,16 @@ const allowedOrigins = config.corsOrigin.split(',').map(origin => origin.trim())
 app.use(rateLimiter);
 app.use(timeout('10s'));
 app.use(helmet());
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
       } else {
-        return callback(new Error('Not allowed by CORS'));
+        callback(new Error('Not allowed by CORS'));
       }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 
