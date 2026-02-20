5aaf6c8a49f098c804ece4f7997a23fd8a93be4d
lovanto <rifkylovanto@gmail.com>
2026-02-21 02:08:02 +0700

    remove sales endpoint

--- src/modules/transactions/routes/index.ts
+++ src/modules/transactions/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import transactionRouter from './transaction.routes';
 import paymentRouter from './payment.routes';
-import salesRouter from './sales.routes';
 
 const router = Router();
 
 router.use('/main', transactionRouter);
 router.use('/payments', paymentRouter);
-router.use('/sales', salesRouter);
 
 export default router;
